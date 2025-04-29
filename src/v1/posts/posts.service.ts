46eb9a39dd67a897fe7448498d5c25769b85b7e3
VasukPasuk <vasapaskivskij@gmail.com>
2025-04-30 00:44:50 +0300

    fix: posts service & post permissions

--- src/v1/posts/posts.service.ts
+++ src/v1/posts/posts.service.ts
@@ -20,20 +20,32 @@ export class PostsService {
   }
 
   async updatePostById(postId: string, post: Partial<CreatePostDto>, userId: string) {
-    await this.checkAccessRightsById(postId, userId)
+    const hasRights = await this.checkAccessRightsById(postId, userId)
+
+    if (!hasRights) {
+      throw new BadRequestException(`You don't have rights to update this post, because you are not the author of this post.`)
+    }
+
     return this.prisma.post.update({where: {id: postId}, data: {...post}});
   }
 
   async deletePostById(postId: string, userId: string) {
-    await this.checkAccessRightsById(postId, userId)
+
+    const hasRights = await this.checkAccessRightsById(postId, userId)
+    if (!hasRights) {
+      throw new BadRequestException(`You don't have rights to delete this post, because you are not the author of this post.`)
+    }
+
     return this.prisma.post.delete({where: {id: postId}});
   }
 
   async checkAccessRightsById(postId: string, userId: string) {
     const post = await this.prisma.post.findUnique({where: {id: postId}})
+
     if (!post) {
       throw new BadRequestException(`Post with id ${postId} not found.`)
     }
+
     return userId === post.authorId;
   }
 }
